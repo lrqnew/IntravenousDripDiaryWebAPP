17c798e03a2e9237c48cb88731e78b5e1cd9b1ce
lrqnew <1406107400@qq.com>
2019-08-31 20:47:16 +0800

    完成登录界面

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ToastService } from 'ng-zorro-antd-mobile';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
