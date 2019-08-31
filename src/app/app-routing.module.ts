40f38c0a5f8b2e74de465bb2ab70615ed9bc19b7
lrqnew <1406107400@qq.com>
2019-08-31 22:43:02 +0800

    完成主页布局

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './components/login/login.component'
-import {RegComponent} from './components/reg/reg.component'
+import {LoginComponent} from './components/login/login.component';
+import {RegComponent} from './components/reg/reg.component';
+import {IndexComponent} from './components/index/index.component'
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},//默认路径
   {path:'login',component:LoginComponent},
-  {path:'reg',component:RegComponent}
+  {path:'reg',component:RegComponent},
+  {path:'index',component:IndexComponent}
 ];
 
 @NgModule({
